707a88a6e87786818b3adb29ae2ba1220fbd5145
LuisNol <nolberto.sumaran@gmail.com>
2022-08-29 17:26:01 -0500

    Actulizacion...

--- src/SEMANA_03/Enunciado_03.java
+++ src/SEMANA_03/Enunciado_03.java
@@ -7,6 +7,18 @@ package SEMANA_03;
  *
  * @author LUIS
  */
+    class Perro {
+         private String name;
+         private String sexo ;
+
+    public Perro(String name, String sexo) {
+        this.name = name;
+        this.sexo = sexo;
+    }
+         
+                 
+                 
+    }
 public class Enunciado_03 {
     public static void main(String[] args) {
         
