b84ad9e03d4338a9b4599d152f0941efaa90f4be
LuisNol <nolberto.sumaran@gmail.com>
2022-08-31 18:15:35 -0500

    CREACION SEMANA 5

---
+++ src/SEMANA_01/SSSS.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package SEMANA_01;
+
+/**
+ *
+ * @author Nolberto Luis Sumaran Pimentel 
+*@phone 930518038
+*  @institutional_mail   nolberto.sumaran@unas.edu.pe
+*  @personal_mail  nolberto.sumaran@gmail.com
+ */
+public class SSSS {
+
+}
